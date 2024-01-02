08f4601b32c27c95fb6a6cbfb38fd1a1171b79ad
Your Name <you@example.com>
2024-01-02 22:51:58 +0700

    update

--- index.js
+++ index.js
@@ -194,7 +194,8 @@ wss.on("connection", async function connection(ws) {
   // check find key by Sec-Websocket-Protocol
   console.log(device);
   const findKey = await key.findById(
-    mongoose.Types.ObjectId(device)
+    // find by _id
+    new mongoose.Types.ObjectId(device)
   )
   console.log(findKey);
   if(findKey !== null && findKey.isActive === true) {
