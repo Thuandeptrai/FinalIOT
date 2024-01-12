d1a4c5b6aab492de09a24966f6fdc8237e4a4c2e
Your Name <you@example.com>
2024-01-12 21:06:52 +0700

    update

--- magnetswitch.ino
+++ magnetswitch.ino
@@ -4,41 +4,44 @@
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <WiFiClientSecure.h>
-
 #include <WebSocketsClient.h>
+#define timeSeconds 10
 #define SWITCH_PIN 22 // ESP32 pin connected to the reed switch
 #define LED_Door 17
 #define LED_restroom 19
 #define LED_bedroom 16
-#define BUTTON_bedroom 13
-#define BUTTON_Door 18
+#define BUTTON_bedroom  13
+#define BUTTON_Door  18
 #define DHTPIN 4
 #define DHTTYPE DHT22
 #define motionSensor 27
-hw_timer_t *timer = NULL;
+#define USE_SERIAL Serial
+hw_timer_t * timer = NULL;
 volatile bool ledState = false;
+// variables will change:
 WiFiMulti WiFiMulti;
 WebSocketsClient webSocket;
 
-// variables will change:
-int led_state_Door = LOW;   // the current state of LED
-int button_state_Door;      // the current state of button
-int last_button_state_Door; // the previous state of button
-int button_state_bedroom;   // the current state of button
+int led_state_Door = LOW;    // the current state of LED
+int button_state_Door;       // the current state of button
+int last_button_state_Door;  // the previous state of button
+int button_state_bedroom;       // the current state of button
 int last_button_state_bedroom;
 // the current state of LED
-// int button_state_restroom;       // the current state of button
-// int last_button_state_restroom;  // the previous state of button
-// settle global delay for system
+//int button_state_restroom;       // the current state of button
+//int last_button_state_restroom;  // the previous state of button
+//settle global delay for system
 unsigned long previousMillis = 0;
-
-const long interval = 1000; // interval at which to blink (milliseconds)
-
+unsigned long previousMillis_restroom = 0;
+unsigned long interval = 1000;// interval at which to blink (milliseconds)
+unsigned long restroom_interval = 8000;
+int led_restroom_state = 0;
 // constants won't change :
 DHT dht(DHTPIN, DHTTYPE);
-
-// Checks if motion was detected, sets LED HIGH and starts a timer
-
+unsigned long lastTrigger = 0;
+boolean startTimer = false;
+boolean motion = false;
+float Flag = 0;
 void timerIsr()
 {
   static unsigned long last_time = millis();
@@ -68,76 +71,55 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
 
   switch (type)
   {
-  case WStype_DISCONNECTED:
-    USE_SERIAL.printf("[WSc] Disconnected!\n");
-    break;
-  case WStype_CONNECTED:
-    USE_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
+    case WStype_DISCONNECTED:
+      USE_SERIAL.printf("[WSc] Disconnected!\n");
+      break;
+    case WStype_CONNECTED:
+      USE_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
 
-    // send message to server when Connected
+      // send message to server when Connected
 
-    break;
-  case WStype_TEXT:
-    USE_SERIAL.printf("[WSc] get text: %s\n", payload);
+      break;
+    case WStype_TEXT:
+      USE_SERIAL.printf("[WSc] get text: %s\n", payload);
 
-    // send message to server
-    // webSocket.sendTXT("message here");
-    break;
-  case WStype_BIN:
-    // // USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
-    // // // hexdump(payload, length);
+      // send message to server
+      // webSocket.sendTXT("message here");
+      break;
+    case WStype_BIN:
+      // // USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
+      // // // hexdump(payload, length);
 
-    // // // send data to server
-    // // // webSocket.sendBIN(payload, length);
-    break;
-  case WStype_PING:
-    // pong will be send automatically
-    USE_SERIAL.printf("[WSc] get ping\n");
-    break;
-  case WStype_PONG:
-    // answer to a ping we send
-    USE_SERIAL.printf("[WSc] get pong\n");
-    break;
-  case WStype_ERROR:
-  case WStype_FRAGMENT_TEXT_START:
-  case WStype_FRAGMENT_BIN_START:
-  case WStype_FRAGMENT:
-  case WStype_FRAGMENT_FIN:
-    break;
+      // // // send data to server
+      // // // webSocket.sendBIN(payload, length);
+      break;
+    case WStype_PING:
+      // pong will be send automatically
+      USE_SERIAL.printf("[WSc] get ping\n");
+      break;
+    case WStype_PONG:
+      // answer to a ping we send
+      USE_SERIAL.printf("[WSc] get pong\n");
+      break;
+    case WStype_ERROR:
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+      break;
   }
 }
-void IRAM_ATTR onTimer()
-{
-  // This function will be called when the timer interrupt is triggered.
-  // Place your code here.
-  timerAlarmEnable(timer);
-  digitalWrite(LED_restroom, LOW); // turn off LED
+// Checks if motion was detected, sets LED HIGH and starts a timer
+void IRAM_ATTR detectsMovement() {
+  digitalWrite(LED_restroom, HIGH);
+  startTimer = true;
+  lastTrigger = millis();
 }
-void setup()
-{
-  pinMode(SWITCH_PIN, INPUT_PULLUP); // initialize digital pin as an input.
-  Serial.begin(115200);              // initialize serial communication at 9600 bits per second
-  // Door setup
-  pinMode(LED_Door, OUTPUT); // set ESP32 pin to output mode
-  pinMode(BUTTON_Door, INPUT_PULLUP);
-  button_state_Door = digitalRead(BUTTON_Door);
-  // restroom setup
-  pinMode(motionSensor, INPUT_PULLUP);
-  // Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
-  pinMode(LED_restroom, OUTPUT); // set ESP32 pin to output mode
-  digitalWrite(LED_restroom, LOW);
-  // bed room setup
-  dht.begin();
-  pinMode(BUTTON_bedroom, INPUT_PULLUP);
-  pinMode(LED_bedroom, OUTPUT);
-  // timer setup
-  timer = timerBegin(0, 80, true);             // Use the first timer, with a prescaler of 80.
-  timerAttachInterrupt(timer, &onTimer, true); // Attach the interrupt function.
-  timerAlarmWrite(timer, 10000000, true);      // Set the alarm value to 1 second (1,000,000 microseconds).
-  timerAlarmEnable(timer);
-  pinMode(LED_Fan, OUTPUT);
-  Serial.print("Connecting to WiFi");
-  WiFi.begin("Thuan", "thuan0023");
+
+void setup() {
+  Serial.begin(115200); // initialize serial communication at 9600 bits per second
+    Serial.print("Connecting to WiFi");
+  WiFi.begin("36 lau 2", "0345616001");
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(100);
@@ -152,7 +134,7 @@ void setup()
   };
 
   // server address, port and URL
-  webSocket.begin("159.223.71.166", 8120, "/", "6rq7g");
+  webSocket.begin("159.223.71.166", 8120, "/", "s55po");
 
   // event handler
   webSocket.onEvent(webSocketEvent);
@@ -162,9 +144,23 @@ void setup()
 
   // try ever 5000 again if connection has failed
   webSocket.setReconnectInterval(5000);
+  //Door setup
+  pinMode(SWITCH_PIN, INPUT_PULLUP); // initialize digital pin as an input.
+  pinMode(LED_Door, OUTPUT); // set ESP32 pin to output mode
+  pinMode(BUTTON_Door, INPUT_PULLUP);
+  button_state_Door = digitalRead(BUTTON_Door);
+  //restroom setup
+  pinMode(motionSensor, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(motionSensor), detectsMovement, RISING);// Set motionSensor pin as interrupt, assign interrupt function and set RISING mode
+  pinMode(LED_restroom, OUTPUT); // set ESP32 pin to output mode
+  digitalWrite(LED_restroom, LOW);
+  //bed room setup
+  dht.begin();
+  pinMode(BUTTON_bedroom, INPUT_PULLUP);
+  pinMode(LED_bedroom, OUTPUT);
+  //timer setup
 }
-void loop()
-{
+void loop() {
   // read the state of the reed switch
   float h = dht.readHumidity();
   float t = dht.readTemperature();
@@ -174,65 +170,31 @@ void loop()
   button_state_Door = digitalRead(BUTTON_Door);
   last_button_state_bedroom = button_state_bedroom;
   button_state_bedroom = digitalRead(BUTTON_bedroom);
-  webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"6rq7g\",\"device4\": " + String(switchState) "}");
-  webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"6rq7g\",\"device5\": " + String(t) "}");
-  webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"6rq7g\",\"device6\": " + String(digitalRead(motionSensor)) "}");
-
   unsigned long now = millis();
-  if (now - previousMillis >= interval)
-  {
+  if (now - previousMillis >= interval) {
     previousMillis = now;
-    // Door scenario
-    if (last_button_state_Door == HIGH && button_state_Door == LOW)
-    {
+    //Door scenario
+    if (last_button_state_Door == HIGH && button_state_Door == LOW) {
       Serial.println("The button is pressed");
     }
-    if (switchState == HIGH || button_state_Door == LOW)
-    { // if door is open or button is pressed
+    if (switchState == HIGH || button_state_Door == LOW) { // if door is open or button is pressed
       Serial.println("The door is open or button is pressed, turns on LED");
-      webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"6rq7g\",\"device1\":  1}");
-
       digitalWrite(LED_Door, HIGH); // turn on LED
-    }
-    else
-    {
+    } else {
       Serial.println("The door is closed and button is not pressed, turns off LED");
-      webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"6rq7g\",\"device1\":  0}");
-
       digitalWrite(LED_Door, LOW); // turn on LED
     }
-    /// End Door scenario
-    /// restroom scenario
-    if (digitalRead(motionSensor) == HIGH)
-    {
-      Serial.println("move!");
-      digitalWrite(LED_restroom, HIGH);
-      webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"6rq7g\",\"device2\":  1}");
-    }
+    ///End Door scenario
 
-    if (digitalRead(motionSensor) == LOW)
-    {
-      Serial.println("dont move!");
-       digitalWrite(LED_restroom, LOW);
-      webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"6rq7g\",\"device2\":  0}");
-      onTimer();
-    }
-    /// End restroom scenario
-    /// Bedroom scenario
-    if (button_state_bedroom == LOW)
-    {                                  // if door is open or button is pressed
+    ///Bedroom scenario
+    if ( (button_state_bedroom == LOW) || (t >= 30)) { // if door is open or button is pressed
       digitalWrite(LED_bedroom, HIGH); // turn on LED
-      webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"6rq7g\",\"device3\":  1}");
-      
-    }
-    else
-    {
+    } else {
       digitalWrite(LED_bedroom, LOW); // turn on LED
-      webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"6rq7g\",\"device3\":  0}");
-
     }
-    if (isnan(h) || isnan(t))
-    {
+    Serial.print(" last_button_state_bedroom:");
+    Serial.print( last_button_state_bedroom);
+    if (isnan(h) || isnan(t)) {
       Serial.println("Failed to read from DHT sensor!");
       return;
     }
@@ -244,6 +206,31 @@ void loop()
     Serial.print(t);
     Serial.println(" *C");
 
-    /// End Bedroom scenario
   }
+  if ((digitalRead(LED_restroom) == HIGH) && (motion == false)) {
+    Serial.println("MOTION DETECTED!!!");
+    motion = true;
+  }
+  if(Flag == 0 ){
+    Flag = t;
+    webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"s55po\",\"device4\": " + String(t)+ "}");
+
+  }
+  if(Flag != t){
+    Flag = t;
+    webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"s55po\",\"device4\": " + String(t)+ "}");
+  }
+  // Turn off the LED after the number of seconds defined in the timeSeconds variable
+  if (startTimer && (now - lastTrigger > (timeSeconds * 1000))) {
+    Serial.println("Motion stopped...");
+    digitalWrite(LED_restroom, LOW);
+    startTimer = false;
+    motion = false;
+  }
+
+  // webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"s55po\",\"device5\": " + String(switchState)+ "}");
+  // webSocket.sendTXT("{ \"type\": \"message\",\"id\": \"s55po\",\"device6\": " + String(digitalRead(motionSensor))+ "}");
+  timerIsr();//  ping server every 1000 milliseconds
+  //webSocket.sendTXT("{\"type\":\"ping\"}\n");
+  webSocket.loop();
 }
